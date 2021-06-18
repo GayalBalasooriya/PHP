c495edaa39c5ad016da6be1bb46f3c09a099f157
Gayal Balasooriya <baigayal01@gmail.com>
2021-06-18 11:36:26 +0530

    minor change

--- 60_61_cookies.php
+++ 60_61_cookies.php
@@ -2,34 +2,29 @@
 
 $name = "SomeName";
 $value = 100;
-$expiration = time() + (60*60*24*7);
-setcookie($name,$value,$expiration);
+$expiration = time() + (60 * 60 * 24 * 7);
+setcookie($name, $value, $expiration);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
+
 <body>
-<?php 
- 
-if(isset($_COOKIE["SomeName"])) {
+    <?php
+    print_r($_COOKIE);
+    if (isset($_COOKIE["SomeName"])) {
+        $someOne = $_COOKIE["SomeName"];
+        echo '<br>'.$someOne;
+    } else {
 
-$someOne = $_COOKIE["SomeName"];
-    
-echo $someOne;
-    
-} else {
-
-$someOne = "";
-
-}
-
-    
-    
-?>
-
+        $someOne = "";
+    }
+    ?>
 
 </body>
+
 </html>
